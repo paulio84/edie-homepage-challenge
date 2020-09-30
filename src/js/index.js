e02c30a657ab74dbee25070ce51eed1626fdf98d
Paul Terry <paul.terry84@gmail.com>
2020-09-30 09:30:25 +0100

    Added scroll behaviour to pageHeader and code cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,7 @@
 const menuTrigger = document.querySelector('#menu-trigger');
-
 menuTrigger.addEventListener('change', triggerNoScroll);
 
 const navLinks = document.querySelectorAll('.navigation__link');
-
 navLinks.forEach(function (nl) {
   nl.addEventListener('click', function () {
     menuTrigger.checked = false;
@@ -13,8 +11,24 @@ navLinks.forEach(function (nl) {
 
 function triggerNoScroll() {
   if (menuTrigger.checked) {
-    document.body.classList.add('noscroll');
+    document.body.classList.add('u-noscroll');
   } else {
-    document.body.classList.remove('noscroll');
+    document.body.classList.remove('u-noscroll');
   }
 }
+
+const contactForms = document.querySelectorAll('.contact-form');
+contactForms.forEach(function (cf) {
+  cf.addEventListener('submit', function (e) {
+    e.preventDefault();
+  });
+});
+
+const pageHeader = document.querySelector('.page-header');
+document.addEventListener('scroll', function () {
+  if (window.pageYOffset >= 50 && !pageHeader.classList.contains('u-bottom-shadow')) {
+    pageHeader.classList.add('u-bottom-shadow');
+  } else if (window.pageYOffset < 50 && pageHeader.classList.contains('u-bottom-shadow')) {
+    pageHeader.classList.remove('u-bottom-shadow');
+  }
+});
